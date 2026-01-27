a297bd4173f70ea094d23ac16ffc9638450a149b
yestoreadcomments-wq <yestoreadcomments@gmail.com>
2026-01-27 13:05:16 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: false,
+
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
 
 module.exports = nextConfig;
